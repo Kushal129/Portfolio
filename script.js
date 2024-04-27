1467a60261459be34ccaecafd48c7048f6dd3f49
kushal129 <21bmiit129@gmail.com>
2024-04-27 13:24:33 +0530

    Dark Light mode add

--- script.js
+++ script.js
@@ -82,3 +82,27 @@ function animateForm() {
 document.getElementById('mobile-menu-toggle').addEventListener('click', function () {
     document.getElementById('nav-links').classList.toggle('hidden');
 });
+
+const toggleSwitch = document.querySelector('#dark-mode-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+if (currentTheme === 'dark') {
+    document.body.classList.add('light-mode');
+    toggleSwitch.checked = true;
+} else {
+    document.body.classList.add('dark-mode'); 
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
